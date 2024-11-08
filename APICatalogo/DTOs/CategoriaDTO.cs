b510e1bada77c3bf9148edcf9aff2a5e77a2caea
Leonardo Dias Correa <leonardo.diascorrea@yahoo.com.br>
2024-11-08 17:02:11 -0300

    Implementação do DTO na entidade Categorias

---
+++ APICatalogo/DTOs/CategoriaDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.DTOs
+{
+    public class CategoriaDTO
+    {
+        public int CategoriaId { get; set; }
+        [Required] // Nome não pode ser nulo 
+        [StringLength(80)] // Nome Pode consumir no máximo 80 bits
+        public string? Nome { get; set; }
+        [Required] // imagemUrl não pode ser nulo   
+        [StringLength(300)] // ImagemUrl pode consumir no máximo 300 bits
+        public string? ImagemUrl { get; set; }
+    }
+}
--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbb23e60e0b5d46fca408ce0db4c61a69d2e111d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61f1c28a99bf34a19fc96c93d4fdd90146c7d9e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
